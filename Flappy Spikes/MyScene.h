b9a7c83ecf6e36267382fbffee941f9e8594a3fc
Hicham Chourak <killerhis@gmail.com>
2014-08-29 15:25:54 +0200

    Initial Commit

---
+++ Flappy Spikes/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Flappy Spikes
+//
+
+//  Copyright (c) 2014 Hicham Chourak. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Flappy SpikesTests/Flappy_SpikesTests.m
@@ -0,0 +1,34 @@
+//
+//  Flappy_SpikesTests.m
+//  Flappy SpikesTests
+//
+//  Created by Hicham Chourak on 29/08/14.
+//  Copyright (c) 2014 Hicham Chourak. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Flappy_SpikesTests : XCTestCase
+
+@end
+
+@implementation Flappy_SpikesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
